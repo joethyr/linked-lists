299662235f6c024dfec163261bcf712344aca894
joethyr <joethayr@gmail.com>
2021-12-12 11:19:23 -0700

    modified append method to remove redundant uses of 'self'.

--- linked_lists.rb
+++ linked_lists.rb
@@ -3,35 +3,33 @@ class LinkedList
   # head acts as a place to start for linkedlist
   attr_accessor :head
 
-  def initialize
+  def initialize(head = nil)
     # when we create a LinkedList we want it to intially be empty
-    self.head = nil
+    @head = head
   end
 
-# adds a new node containing value to the end of the list
+  # adds a new node containing value to the end of the list
   def append(value)
-    if self.head.nil?
-      self.head = Node.new(value)
+    if head.nil?
+      @head = Node.new(value)
     else
       # find the last node in LinkedList and set the next node equal to a new node.
-      last_node = self.head
-      until last_node.next_node.nil?
-        last_node = last_node.next_node
-      end
+      last_node = head
+      last_node = last_node.next_node until last_node.next_node.nil?
+
       # we are at the end of the list.
       last_node.next_node = Node.new(value)
     end
   end
 
   private
-  # only going to be used inside the LinkedList
 
   class Node
     attr_accessor :value, :next_node
 
     def initialize(value = nil, next_node = nil)
-      self.value = value
-      self.next_node = next_node
+      @value = value
+      @next_node = next_node
     end
   end
 end
