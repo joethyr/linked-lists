50b523fbbc1d0e82ce1527b4dea175ce26ff2b9f
joethyr <joethayr@gmail.com>
2021-12-12 09:31:06 -0700

    Inital commit.

---
+++ linked_lists.rb
@@ -0,0 +1,10 @@
+#
+class LinkedList
+  # head acts as a place to start for linkedlist
+  attr_accessor :head
+
+  def initialize
+    # when we create a LinkedList we want it to intially be empty
+    self.head = nil
+  end
+end
