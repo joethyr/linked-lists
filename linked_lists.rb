1af09bc62a1df55814f498eb1bc353b4d9d76b11
joethyr <joethayr@gmail.com>
2021-12-12 09:42:39 -0700

    added Node class within private method.

--- linked_lists.rb
+++ linked_lists.rb
@@ -7,4 +7,16 @@ class LinkedList
     # when we create a LinkedList we want it to intially be empty
     self.head = nil
   end
+
+  private
+  # only going to be used inside the LinkedList
+
+  class Node
+    attr_accessor :value, :next_node
+
+    def initialize(value, next_node)
+      self.value = value
+      self.next_node = next_node
+    end
+  end
 end
