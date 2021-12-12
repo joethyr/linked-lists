0a823057ae3b910e43ec058c03804a0a32932939
joethyr <joethayr@gmail.com>
2021-12-12 10:27:45 -0700

    Added append method that adds a new node to linkedList.

--- linked_lists.rb
+++ linked_lists.rb
@@ -8,15 +8,36 @@ class LinkedList
     self.head = nil
   end
 
+# adds a new node containing value to the end of the list
+  def append(value)
+    if self.head.nil?
+      self.head = Node.new(value)
+    else
+      # find the last node in LinkedList and set the next node equal to a new node.
+      last_node = self.head
+      until last_node.next_node.nil?
+        last_node = last_node.next_node
+      end
+      # we are at the end of the list.
+      last_node.next_node = Node.new(value)
+    end
+  end
+
   private
   # only going to be used inside the LinkedList
 
   class Node
     attr_accessor :value, :next_node
 
-    def initialize(value, next_node)
+    def initialize(value = nil, next_node = nil)
       self.value = value
       self.next_node = next_node
     end
   end
 end
+
+ll = LinkedList.new
+ll.append(10)
+ll.append(20)
+puts ll.head.value
+puts ll.head.next_node.value
